15769a46765224a9b724b0ec32429275568ff16c
securar <ilya.pro008@gmail.com>
2025-09-09 21:58:26 +0300

    format simple showcase

--- examples/simple_showcase.py
+++ examples/simple_showcase.py
@@ -2,11 +2,13 @@ from measuring import Measurer
 
 measurer = Measurer()
 
+
 @measurer.func
 def loop_func() -> None:
     for _ in range(5_000_000):
         continue
-    
+
+
 if __name__ == "__main__":
     with measurer.region("long loop"):
         loop_func()
